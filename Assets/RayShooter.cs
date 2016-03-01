9da7f9fd3f9d98dbe0a1d1e721dfec5d04ec28b9
Danny Tizon <dtizon@gmail.com>
2016-02-29 21:25:27 -0800

    added onGUI to get crosshair

--- Assets/RayShooter.cs
+++ Assets/RayShooter.cs
@@ -7,6 +7,9 @@ public class RayShooter : MonoBehaviour {
 
 	void Start () {
 		_camera = GetComponent<Camera> ();
+
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
 	}
 		
 	void Update () {
@@ -18,6 +21,13 @@ public class RayShooter : MonoBehaviour {
 				StartCoroutine(SphereIndicator(hit.point));
 			}
 		}
+	}
+
+	void OnGUI() {
+		int size = 12;
+		float posX = _camera.pixelWidth/2 - size/4;
+		float posY = _camera.pixelHeight/2 - size/2;
+		GUI.Label(new Rect(posX, posY, size, size), "+");
 	}
 
 	private IEnumerator SphereIndicator (Vector3 pos){
