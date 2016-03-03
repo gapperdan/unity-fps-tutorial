73458fec136c3b0fd69be12cddb53067c2b41107
Danny Tizon <dtizon@gmail.com>
2016-03-02 22:11:40 -0800

    ReactiveTarget implemented

--- Assets/RayShooter.cs
+++ Assets/RayShooter.cs
@@ -11,17 +11,6 @@ public class RayShooter : MonoBehaviour {
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
 	}
-		
-	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
-			Vector3 point = new Vector3 (_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
-			Ray ray = _camera.ScreenPointToRay (point);
-			RaycastHit hit;
-			if (Physics.Raycast (ray, out hit)) {
-				StartCoroutine(SphereIndicator(hit.point));
-			}
-		}
-	}
 
 	void OnGUI() {
 		int size = 12;
@@ -29,7 +18,24 @@ public class RayShooter : MonoBehaviour {
 		float posY = _camera.pixelHeight/2 - size/2;
 		GUI.Label(new Rect(posX, posY, size, size), "+");
 	}
-
+		
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Vector3 point = new Vector3 (_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
+			Ray ray = _camera.ScreenPointToRay (point);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit)) {
+				GameObject hitObject = hit.transform.gameObject;
+				ReactiveTarget target = hitObject.GetComponent<ReactiveTarget>();
+				if (target != null) {
+					target.ReactToHit();
+				} else {
+					StartCoroutine(SphereIndicator(hit.point));
+				} 
+			}
+		}
+	}
+		
 	private IEnumerator SphereIndicator (Vector3 pos){
 		GameObject sphere = GameObject.CreatePrimitive (PrimitiveType.Sphere);
 		sphere.transform.position = pos;
