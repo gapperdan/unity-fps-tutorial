7d3e833f6cd38405c707d245fd43705c83fb2a25
Danny Tizon <dtizon@gmail.com>
2016-03-14 11:04:34 -0700

    added popup script

---
+++ Assets/Scripts/SettingsPopup.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingsPopup : MonoBehaviour {
+	public void Open() {
+		gameObject.SetActive(true);
+	}
+	public void Close() {
+		gameObject.SetActive(false);
+	}
+}
